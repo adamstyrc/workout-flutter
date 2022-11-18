8929305b5535329b2e57d86573fe8ed53d695bbc
astyrc <styrc.adam@gmail.com>
2022-11-18 10:41:46 +0100

    Setup project with GetX

---
+++ lib/modules/home/views/home_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text('+'),
+      ),
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'HomeView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
